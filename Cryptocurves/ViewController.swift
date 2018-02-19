e5f8aff9895bbb7097090adb5e8352d481db3281
Justin Hershey <justin.hershey@gmail.com>
2018-02-19 10:27:30 -0500

    Initial Commit

--- Cryptocurves/ViewController.swift
+++ Cryptocurves/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    /*****************************
+     *
+     *  TABLEVIEW DATA SOURCE
+     *
+     ****************************/
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        return cell
+    }
+    
 
 }
 
